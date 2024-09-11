5a4074a65d0b1007a6ec8289183e8c24b70bb959
ghostclog <kdy11281@gmail.com>
2024-09-11 16:40:47 +0900

    12장 공부 내용 업로드

--- Assingment/Assginment_7/app.py
+++ Assingment/Assginment_7/app.py
@@ -1,14 +1,13 @@
-import json
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import SitemapLoader
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
-from langchain.callbacks import StreamingStdOutCallbackHandler
 import streamlit as st
-from langchain.retrievers import WikipediaRetriever
-from langchain.schema import BaseOutputParser, output_parser
 
-from functions import wikipedia_search,split_file,run_quiz_chain,make_llm
+from functions import make_llm,make_retriever
 
 docs = None
 choice = None
@@ -26,9 +25,7 @@ st.markdown("""
             
             1. 사이드탭에 api키를 입력해주세요!
 
-            2. 확인하실 사이트 링크를 알려주세요!
-
-            3. GPT가 사이트를 다 확인하면, 해당 사이트에 대한 질문을 해주세요!
+            2. GPT가 사이트를 다 확인하면, 해당 사이트(cloudflare)에 대한 질문을 해주세요!
             """)
 
 
@@ -39,18 +36,198 @@ with st.sidebar:
         
     #분할선
     st.divider()
-    st.write("레포지토리 링크: ")
+    st.write("레포지토리 링크: https://github.com/ghostclog/nomad-gpt-ch10-Assignment")
     st.divider()
     st.header("app.py 내용")
     st.markdown("""
+ docs = None
+choice = None
+correct_pt = 0
+num_of_question = 0
+st.set_page_config(
+    page_title="Assignment 7",
+    page_icon="❓",
+)
+
+st.title("Assignment 7")
+
+st.markdown('''
+            GPT 과제 7번입니다!
+            
+            1. 사이드탭에 api키를 입력해주세요!
+
+            2. GPT가 사이트를 다 확인하면, 해당 사이트(cloudflare)에 대한 질문을 해주세요!
+            ''')
+
+
+with st.sidebar:
+    api_key = st.text_input("하단에 api 키를 입력해주세요.")
+    if api_key:
+        make_llm(api_key)               
+
+if api_key:
+    st.write("")
+    st.write("")
+    make_retriever()
     """)
     st.divider()
     st.header("function.py 내용입니다.")
     st.markdown("""
+    from langchain.document_loaders import SitemapLoader
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+import streamlit as st
+
+llm = None
+
+def make_llm(key):
+    global llm
+    llm = ChatOpenAI(
+        temperature=0.5,
+        streaming=True,
+        openai_api_key=key
+        )
+
+answers_prompt = ChatPromptTemplate.from_template(
+    '''
+    다음 컨텍스트만 사용하면 사용자의 질문에 답할 수 있습니다. 모른다고만 말할 수 없다면 아무것도 지어내지 마세요.
+                                                    
+    그런 다음 0에서 5 사이의 답에 점수를 부여합니다.
+
+    답변이 사용자에게 높은 점수를 요구하면 낮은 점수를 받아야 합니다.
+
+    0인 경우에도 항상 정답의 점수를 포함해야 합니다.
+
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    질문: 달은 얼마나 멀리 있나요?
+    답변: 달은 384,400km 떨어져 있습니다.
+    점수: 5
+                                                  
+    질문: 태양은 얼마나 멀리 있나요?
+    답변: 모르겠습니다
+    점수: 0
+                                                  
+    네 차례!
+
+    Question: {question}
+'''
+)
+
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+    return {
+        "question": question,
+        "answers": [
+            {
+                "answer": answers_chain.invoke(
+                    {"question": question, "context": doc.page_content}
+                ).content,
+                "source": doc.metadata["source"],
+                "date": doc.metadata["lastmod"],
+            }
+            for doc in docs
+        ],
+    }
+
+
+choose_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            '''
+            다음 기존 답변만 사용하여 사용자의 질문에 답하세요.
+
+            가장 높은 점수(더 도움이 되는)를 받고 가장 최근의 점수를 선호하는 답을 사용합니다.
+
+            출처를 인용하고 답변의 출처를 변경하지 말고 그대로 반환합니다.
+
+            Answers: {answers}
+            ''',
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+
+def choose_answer(inputs):
+    answers = inputs["answers"]
+    question = inputs["question"]
+    choose_chain = choose_prompt | llm
+    condensed = "\n\n".join(
+        f"{answer['answer']}\nSource:{answer['source']}\nDate:{answer['date']}\n"
+        for answer in answers
+    )
+    return choose_chain.invoke(
+        {
+            "question": question,
+            "answers": condensed,
+        }
+    )
+
+
+def parse_page(soup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return (
+        str(soup.get_text())
+        .replace("\n", " ")
+        .replace("\xa0", " ")
+        .replace("CloseSearch Submit Blog", "")
+    )
+
+
+@st.cache_data(show_spinner="웹사이트 로딩 중...")
+def load_website(url):
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    loader = SitemapLoader(
+        url,
+        parsing_function=parse_page,
+        filter_urls=[
+            r"^(.*\/ai-gateway\/).*",
+            r"^(.*\/vectorize\/).*",
+            r"^(.*\/workers-ai\/).*",
+        ],
+    )
+    loader.requests_per_second = 2
+    docs = loader.load_and_split(text_splitter=splitter)
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
+
+def make_retriever():
+    retriever = load_website("https://developers.cloudflare.com/sitemap-0.xml")
+    query = st.text_input("해당 웹사이트에 대해 물어보세요.")
+    if query:
+        chain = (
+            {
+                "docs": retriever,
+                "question": RunnablePassthrough(),
+            }
+            | RunnableLambda(get_answers)
+            | RunnableLambda(choose_answer)
+        )
+        result = chain.invoke(query)
+        st.markdown(result.content.replace("$", "\$"))
     """)
 
 
-if docs:
+if api_key:
     st.write("")
     st.write("")
-    
+    make_retriever()
--- Assingment/Assginment_7/functions.py
+++ Assingment/Assginment_7/functions.py
@@ -1,201 +1,11 @@
-import json
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import SitemapLoader
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
-from langchain.callbacks import StreamingStdOutCallbackHandler
 import streamlit as st
-from langchain.retrievers import WikipediaRetriever
-from langchain.schema import BaseOutputParser, output_parser
-
-easy_questions_prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                """
-    당신은 문제를 만들어주는 ai입니다. 사용자가 데이터를 당신에게 제공한다면, 당신은 반드시 4개 이상의 문제를 만들어내야 합니다.
-
-    난이도의 경우 반드시 "쉬움"로 맞춰야하며, 각 질문에는 4개의 답이 있어야 하고, 그 중 3개는 틀려야 하며, 1개는 정답이여야합니다.
-
-    (o)를 사용하여 정답을 알립니다.
-         
-    질문 예제:
-         
-    질문: 바다의 색은 무엇인가요?
-    답변: 빨간색|노란색|녹색|파란색(o)
-         
-    질문: 수도 또는 조지아는 어디인가요?
-    답변: 바쿠|트빌리시(o)|마닐라|베이루트
-         
-    질문: 아바타는 언제 출시되었나요?
-    답변: 2007|2001|2009(o)|1998
-         
-    질문: 줄리어스 시저는 누구였나요?
-    답변: 로마 황제(o)|화가|배우|모델
-         
-    네 차례!
-         
-    Context: {context}
-""",
-        )
-    ]
-)
-
-hard_questions_prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                """
-    당신은 문제를 만들어주는 ai입니다. 사용자가 데이터를 당신에게 제공한다면, 당신은 반드시 4개 이상의 문제를 만들어내야 합니다.
-
-    난이도의 경우 반드시 "어려움"로 맞춰야하며, 각 질문에는 4개의 답이 있어야 하고, 그 중 3개는 틀려야 하며, 1개는 정답이여야합니다.
-
-    (o)를 사용하여 정답을 알립니다.
-         
-    질문 예제:
-         
-    질문: 바다의 색은 무엇인가요?
-    답변: 빨간색|노란색|녹색|파란색(o)
-         
-    질문: 수도 또는 조지아는 어디인가요?
-    답변: 바쿠|트빌리시(o)|마닐라|베이루트
-         
-    질문: 아바타는 언제 출시되었나요?
-    답변: 2007|2001|2009(o)|1998
-         
-    질문: 줄리어스 시저는 누구였나요?
-    답변: 로마 황제(o)|화가|배우|모델
-         
-    네 차례!
-         
-    Context: {context}
-""",
-        )
-    ]
-)
-
-formatting_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-    당신은 강력한 포맷 알고리즘입니다.
-
-    시험 문제를 JSON 형식으로 포맷합니다.
-    (o)로 답하는 것이 맞습니다.
-     
-    예시 입력:
-
-    Question: 바다의 색상으로 가장 적절한 것은?
-    Answers: 빨강|노랑|초록|파랑(o)
-         
-    Question: 조지아의 수도는?
-    Answers: 바쿠|트빌리시(o)|마닐라|베이루트
-         
-    Question: 영화 '아바타'의 개봉 연도는?
-    Answers: 2007|2001|2009(o)|1998
-         
-    Question: 시저는 누구인가?
-    Answers: 로마 황제(o)|화가|배우|모델
-    
-     
-    예시 출력:
-     
-    ```json
-    {{ "questions": [
-            {{
-                "question": "바다의 색상으로 가장 적절한 것은?",
-                "answers": [
-                        {{
-                            "answer": "빨강",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "노랑",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "초록",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "파랑",
-                            "correct": true
-                        }},
-                ]
-            }},
-                        {{
-                "question": "조지아의 수도는?",
-                "answers": [
-                        {{
-                            "answer": "바쿠",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "트빌리시",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "마닐라",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "베이루트",
-                            "correct": false
-                        }},
-                ]
-            }},
-                        {{
-                "question": "영화 '아바타'의 개봉 연도는?",
-                "answers": [
-                        {{
-                            "answer": "2007",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "2001",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "2009",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "1998",
-                            "correct": false
-                        }},
-                ]
-            }},
-            {{
-                "question": "시저는 누구인가?",
-                "answers": [
-                        {{
-                            "answer": "로마 황제",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "화가",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "배우",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "모델",
-                            "correct": false
-                        }},
-                ]
-            }}
-        ]
-     }}
-    ```
-    Your turn!
-    Questions: {context}
-""",
-        )
-    ]
-)
 
 llm = None
 
@@ -207,43 +17,135 @@ def make_llm(key):
         openai_api_key=key
         )
 
-class JsonOutputParser(BaseOutputParser):
-    def parse(self, text):
-        text = text.replace("```", "").replace("json", "")
-        return json.loads(text)
-    
-output_parser = JsonOutputParser()
+answers_prompt = ChatPromptTemplate.from_template(
+    """
+    다음 컨텍스트만 사용하면 사용자의 질문에 답할 수 있습니다. 모른다고만 말할 수 없다면 아무것도 지어내지 마세요.
+                                                    
+    그런 다음 0에서 5 사이의 답에 점수를 부여합니다.
 
-def format_docs(docs):
-    return "\n\n".join(document.page_content for document in docs)
+    답변이 사용자에게 높은 점수를 요구하면 낮은 점수를 받아야 합니다.
 
-@st.cache_data(show_spinner="위키피디아 검색 중...")
-def wikipedia_search(term):
-    retriver = WikipediaRetriever(top_k_results=3, lang="ko")
-    return retriver.get_relevant_documents(term)
+    0인 경우에도 항상 정답의 점수를 포함해야 합니다.
 
-@st.cache_data(show_spinner="파일 로딩중...")
-def split_file(file):
-    file_content = file.read()
-    file_path = f"./.cache/quiz_files/{file.name}"
-    with open(file_path, "wb") as f:
-        f.write(file_content)
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="\n",
-        chunk_size=600,
-        chunk_overlap=100,
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    질문: 달은 얼마나 멀리 있나요?
+    답변: 달은 384,400km 떨어져 있습니다.
+    점수: 5
+                                                  
+    질문: 태양은 얼마나 멀리 있나요?
+    답변: 모르겠습니다
+    점수: 0
+                                                  
+    네 차례!
+
+    Question: {question}
+"""
+)
+
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+    return {
+        "question": question,
+        "answers": [
+            {
+                "answer": answers_chain.invoke(
+                    {"question": question, "context": doc.page_content}
+                ).content,
+                "source": doc.metadata["source"],
+                "date": doc.metadata["lastmod"],
+            }
+            for doc in docs
+        ],
+    }
+
+
+choose_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            다음 기존 답변만 사용하여 사용자의 질문에 답하세요.
+
+            가장 높은 점수(더 도움이 되는)를 받고 가장 최근의 점수를 선호하는 답을 사용합니다.
+
+            출처를 인용하고 답변의 출처를 변경하지 말고 그대로 반환합니다.
+
+            Answers: {answers}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+
+def choose_answer(inputs):
+    answers = inputs["answers"]
+    question = inputs["question"]
+    choose_chain = choose_prompt | llm
+    condensed = "\n\n".join(
+        f"{answer['answer']}\nSource:{answer['source']}\nDate:{answer['date']}\n"
+        for answer in answers
     )
-    loader = UnstructuredFileLoader(file_path)
+    return choose_chain.invoke(
+        {
+            "question": question,
+            "answers": condensed,
+        }
+    )
+
+
+def parse_page(soup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return (
+        str(soup.get_text())
+        .replace("\n", " ")
+        .replace("\xa0", " ")
+        .replace("CloseSearch Submit Blog", "")
+    )
+
+
+@st.cache_data(show_spinner="웹사이트 로딩 중...")
+def load_website(url):
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    loader = SitemapLoader(
+        url,
+        parsing_function=parse_page,
+        filter_urls=[
+            r"^(.*\/ai-gateway\/).*",
+            r"^(.*\/vectorize\/).*",
+            r"^(.*\/workers-ai\/).*",
+        ],
+    )
+    loader.requests_per_second = 2
     docs = loader.load_and_split(text_splitter=splitter)
-    return docs
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
 
-@st.cache_data(show_spinner="퀴즈 만드는중...")
-def run_quiz_chain(_docs,nan_e_do):
-    formatting_chain = formatting_prompt | llm
-    if nan_e_do == "쉬움":
-        questions_chain  = {"context": format_docs} | easy_questions_prompt | llm
-    elif nan_e_do == "어려움":
-        questions_chain  = {"context": format_docs} | hard_questions_prompt | llm
-
-    chain = {"context": questions_chain} | formatting_chain | output_parser
-    return chain.invoke(_docs)
+def make_retriever():
+    retriever = load_website("https://developers.cloudflare.com/sitemap-0.xml")
+    query = st.text_input("해당 웹사이트에 대해 물어보세요.")
+    if query:
+        chain = (
+            {
+                "docs": retriever,
+                "question": RunnablePassthrough(),
+            }
+            | RunnableLambda(get_answers)
+            | RunnableLambda(choose_answer)
+        )
+        result = chain.invoke(query)
+        st.markdown(result.content.replace("$", "\$"))
